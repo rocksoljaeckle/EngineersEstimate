f0f281adec987bc41c90703e3074c23456e9656c
Louis <jaeckle@rocksol.com>
2025-12-18 13:20:19 -0700

    debug statement

--- estimate_app.py
+++ estimate_app.py
@@ -465,6 +465,7 @@ def render_estimate_table():
 print('debug statements:')
 print(os.listdir('.')) # TODO REMOVE
 print(os.listdir('prompts')) # TODO REMOVE
+print(os.listdir('assets')) # TODO REMOVE
 if 'config' not in st.session_state:
     with open('config.toml', 'rb') as f:
         st.session_state['config'] = tomli.load(f)
